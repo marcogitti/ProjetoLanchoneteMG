d8c2508e395b496a20d0300f02c456d3602565c3
marcogitti <marcoalcindogitti@gmail.com>
2023-05-17 16:18:41 -0300

    Arquivos salvos e adicionados

---
+++ LanchoneteProjetoMG/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LanchoneteProjetoMG.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
